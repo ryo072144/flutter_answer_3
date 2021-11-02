8c46c1c1035113371032349f5c8630af6a6e0a2c
ryo072144 <ryonakahara0935@gmail.com>
2021-11-02 14:01:52 +0900

    minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -41,10 +41,19 @@ class _MyHomePageState extends State<MyHomePage> {
 
           //条件式?値1:値2で、if(条件式){return 値1;}else{return 値2;}と同じ意味。
           child: Icon(_isToggleOn?Icons.toggle_on_outlined:Icons.toggle_off_outlined, color: Colors.white, size: 40,),
-          onPressed: (){},
+          onPressed: (){
+
+            //問４： _isToggleOnの値を逆にすれば良い。
+            setState(() {
+              _isToggleOn = !_isToggleOn;
+            });
+          },
         ),
         title: Text('タイトル', style: titleStyle(),),
       ),
+
+      //問５： _isToggleOnの値に応じて、色を変化させる。
+      backgroundColor: _isToggleOn?Colors.white:Colors.black,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -58,7 +67,13 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                  onPressed: (){},
+                  onPressed: (){
+
+                    //問２： _counterの値を１減らして、画面を更新。
+                    setState(() {
+                      _counter--;
+                    });
+                  },
                   child: const Icon(Icons.remove),
                   style: ButtonStyle(
                       padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
@@ -66,8 +81,29 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                 ),
                 const SizedBox(width: 20,),
+
+                //問３： _counterの値を０にして、画面を更新。
                 ElevatedButton(
-                  onPressed: (){},
+                  onPressed: (){
+                    setState(() {
+                      _counter=0;
+                    });
+                  },
+                  child: const Text('Reset', style: TextStyle(color: Colors.white),),
+                  style: ButtonStyle(
+                      padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)))
+                  ),
+                ),
+                const SizedBox(width: 20,),
+                ElevatedButton(
+                  onPressed: (){
+                    setState(() {
+
+                      //問２： _counterの値を１増やして、画面を更新。
+                      _counter++;
+                    });
+                  },
                   child: const Icon(Icons.add),
                   style: ButtonStyle(
                       padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
