cdc08da6d50453ca092321c488528fc6caffda5a
ryo072144 <84168405+ryo072144@users.noreply.github.com>
2022-07-27 13:48:10 +0900

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const TextStyle titleStyle = TextStyle(color: Color(0xffeeeeee), fontSize: 20 ,fontWeight: FontWeight.bold);
